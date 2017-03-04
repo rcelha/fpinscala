7e6df22bf5d6c5cb00be32785be21c66b45a1f73
Rodrigo Corrêa <rcelha@gmail.com>
2017-03-04 23:33:14 +0000

    Complete errorhandling 01

---
+++ exercises/src/main/scala/fpinscala/errorhandling/Main.scala
@@ -0,0 +1,34 @@
+package fpinscala.errorhandling
+
+object Main {
+  def main (args: Array[String]) {
+    println("# Error Handling")
+
+    println("## Options")
+    println(Some("oi"))
+
+    println("### map")
+    println(Some("map").map(x => x + " transformed"))
+    val opt1: Option[String] = None
+    println(opt1.map(x => x + " transformed"))
+
+    println("### getOrElse")
+    println(Some("map").getOrElse("------"))
+    println(opt1.getOrElse("------"))
+
+    println("### flatMap")
+    println(Some("flat").flatMap(x => Some(x + " transformed")))
+    println(opt1.flatMap(x => Some(x + " transformed")))
+
+    println("### orElse")
+    println(Some("orElse") orElse Some("orElse?"))
+    println(opt1 orElse Some("orElse?"))
+
+    println("### filter")
+    val opt2: Option[Integer] = None
+    println(Some(100) filter(_ > 10))
+    println(Some(9) filter(_ > 10))
+    println(opt2 filter(_ > 10))
+
+  }
+}
