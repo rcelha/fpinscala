58165d2b994cbc11b7904fbb972f7bb5cbec53d0
Rodrigo Corrêa <rcelha@gmail.com>
2017-03-17 00:29:19 +0000

    Finish up to ex5 in laziness chapter

---
+++ exercises/src/main/scala/fpinscala/laziness/Main.scala
@@ -0,0 +1,31 @@
+package fpinscala.laziness
+
+object Main {
+  def main (args: Array[String]) {
+    println("# Strictness and Laziness")
+
+    val intStream = Stream(1,2,3,4,5,6)
+    println(intStream)
+
+    println("## toList")
+    println(intStream.toList)
+
+    println("## take")
+    println((Empty: Stream[Integer]) take 0 toList)
+    println(intStream take 0 toList)
+    println(intStream take 1 toList)
+    println(intStream take 3 toList)
+    println(intStream take 5 toList)
+
+    println("## takeWhile")
+    println(intStream takeWhile (_ < 5) toList)
+
+    println("## forAll")
+    println(intStream forAll (_ < 10))
+    println(intStream forAll (_ < 3))
+
+    println("## foldedTakeWhile")
+    println(intStream foldedTakeWhile (_ < 5) toList)
+    println(intStream foldedTakeWhile (_ < 50) toList)
+  }
+}
--- exercises/src/main/scala/fpinscala/laziness/Stream.scala
+++ exercises/src/main/scala/fpinscala/laziness/Stream.scala
@@ -2,7 +2,6 @@ package fpinscala.laziness
 
 import Stream._
 trait Stream[+A] {
-
   def foldRight[B](z: => B)(f: (A, => B) => B): B = // The arrow `=>` in front of the argument type `B` means that the function `f` takes its second argument by name and may choose not to evaluate it.
     this match {
       case Cons(h,t) => f(h(), t().foldRight(z)(f)) // If `f` doesn't evaluate its second argument, the recursion never occurs.
@@ -17,13 +16,34 @@ trait Stream[+A] {
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
   }
-  def take(n: Int): Stream[A] = sys.error("todo")
+
+  def toList: List[A] =
+    this match {
+      case Empty => List()
+      case Cons(h, t) => h() :: t().toList
+    }
+
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n == 1 => cons(h(), empty)
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n-1))
+    case _ => Empty
+  }
 
   def drop(n: Int): Stream[A] = sys.error("todo")
 
-  def takeWhile(p: A => Boolean): Stream[A] = sys.error("todo")
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if p(h()) => cons(h(), t().takeWhile(p))
+    case Cons(h, t) if !p(h()) => cons(h(), empty)
+    case _ => Empty
+  }
 
-  def forAll(p: A => Boolean): Boolean = sys.error("todo")
+  def foldedTakeWhile(p: A => Boolean): Stream[A] =
+    foldRight(Empty:Stream[A])((a,b) =>
+        if (p(a)) cons(a, b)
+        else empty)
+
+  def forAll(p: A => Boolean): Boolean =
+    foldRight(true)((a,b) => p(a) && b)
 
   def headOption: Option[A] = sys.error("todo")
 
